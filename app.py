f7234696676d56993c72ec88f9b839feb231868e
Sameer Khan <www.sameerkhan090912@gmail.com>
2024-12-10 20:44:53 +0530

    Inital Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def Hello():
+    name="sameer_Khan"
+    return render_template("index.html",first_name=name)
+
+@app.route("/user/<name>")
+def User(name):
+    return render_template("base.html",name=name)
+
+@app.route("/user/jinja")
+def jinjaFilter():
+    fav_fruit=["apple","oranges",3]
+    sports=["running","boxing","kabbadi","kho-kho","cricket"]
+    name=["sameer","tanveer","faizan",41]
+    stuff="this is the <strong>Bold</strong> tag"
+    return render_template("jinjaFilter.html",stuff=stuff,fav_fruit=fav_fruit,sports=sports,name=name)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
